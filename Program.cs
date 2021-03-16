796e57ced11bb3ecc7add56e239e81185e856082
Sanches <autoscript@gmail.com>
2021-03-16 12:38:11 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿    using System;
+
+namespace test_val_ref_static
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.ReadKey();
+        }
+        
+    }
+    
+    
+
+
+}
